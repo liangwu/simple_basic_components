a1b6e8c3e3da2ca51abd7455495a816898b50499
lixingyu <liangwu.lxy@foxmail.com>
2023-03-29 23:06:41 +0800

    添加extern"C"，对cpp的兼容

--- include/queue.h
+++ include/queue.h
@@ -12,10 +12,15 @@ struct Queue{
 	struct Node *head;
 };
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 int queue_create(struct Queue **queue);
 int queue_push(struct Queue *queue, void *data);
 void* queue_pop(struct Queue *queue);
 void queue_destroy(struct Queue **queue);
-
+#ifdef __cplusplus
+}
+#endif
 
 #endif // !_QUEUE_H_
