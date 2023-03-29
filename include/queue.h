c71fb97adc301e4c6d02dfad3ca48cc52ff4319c
lixingyu <liangwu.lxy@foxmail.com>
2023-03-29 21:57:01 +0800

    add queue.* rbtree.c

---
+++ include/queue.h
@@ -0,0 +1,21 @@
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+struct Node
+{
+	struct Node *prev;
+	struct Node *next;
+	void *data;
+};
+
+struct Queue{
+	struct Node *head;
+};
+
+int queue_create(struct Queue **queue);
+int queue_push(struct Queue *queue, void *data);
+void* queue_pop(struct Queue *queue);
+void queue_destroy(struct Queue **queue);
+
+
+#endif // !_QUEUE_H_
